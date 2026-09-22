412c7fdd23b1373a7bbc1db0f19ec45a678543df
Oscar Sorensen <oscar081100@gmail.com>
2026-09-22 17:41:55 +0200

    class

---
+++ 2-DWE-S/004-Desarrollo-de-aplicaciones-web-utilizando-código-embebido/001-Mantenimiento-del-estado/001-archivo1.php
@@ -0,0 +1,4 @@
+<?php
+	$edad = 48;
+?>
+<a href="002-archivo2.php">Vamos al archivo 2</a>
---
+++ 2-DWE-S/004-Desarrollo-de-aplicaciones-web-utilizando-código-embebido/001-Mantenimiento-del-estado/003-sesion-inicio.php
@@ -0,0 +1,5 @@
+<?php
+	session_start();
+  $_SESSION['edad'] = 48;
+?>
+<a href="004-archivo4.php">Vamos al archivo 2</a>
